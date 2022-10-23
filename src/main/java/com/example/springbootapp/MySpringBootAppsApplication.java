f4fa2963b7bfb48b225a159fa5a5475af8c87dfd
Kotaii <ronaldkwong@RONALDs-MacBook-Air.local>
2022-10-23 14:48:38 -0700

    Second Commit

---
+++ src/main/java/com/example/springbootapp/MySpringBootAppsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootAppsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootAppsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootapp/MySpringBootAppsApplicationTests.java
@@ -0,0 +1,21 @@
+package com.example.springbootapp;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringBootAppApplicationTests {
+
+	@Autowired
+	NewsController newsController;
+	
+	@Test
+	void contextLoads() {
+	    assertThat(newsController).isNotNull();
+	
+	}
+
+}
